3eecbd369ee2cd640ccf189482f216a457333844
Shangwei.Tsai <shangwei.tsai@tripla.jp>
2022-01-19 21:09:43 +0800

    nice

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,13 @@ import Layout from "../components/Layout";
 export default function About() {
   return (
     <Layout>
-        <h1>About A Design Di</h1>
+      <section id="top" className="bg-main relative">
+        <div className="content-container content-section pb40 pt40 ph80 d-flex flex-column justify-content-center">
+          <h1>About A Design Di</h1>
+          <div style={{ height: 30 }} />
+          <img src="/profpic_regular 1.png" />
+        </div>
+      </section>
     </Layout>
   );
 }
