0a362e550842be207561062c14422330943e2a69
Shangwei.Tsai <shangwei.tsai@tripla.jp>
2022-01-20 22:17:10 +0800

    fix typo

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
     <Layout>
       <section id="top" className="bg-main relative">
         <div className="content-container content-section pb40 pt40 ph80 d-flex flex-column justify-content-center">
-          <h1>About A Design Di</h1>
+          <h1>About A Design Dee</h1>
           <div style={{ height: 30 }} />
           <img className="mw520" src="/profpic_regular 1.png" />
         </div>
